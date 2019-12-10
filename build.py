18e0912e2439fcf51aec9c387b70e23a404a9f69
Ammon Smith <ammon.i.smith@gmail.com>
2019-12-10 00:25:47 -0500

    Add style.css file.

--- build.py
+++ build.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
+    with open('style.css') as file:
+        style = file.read()
+
     with open('script.js') as file:
         script = file.read()
 
     with open('page.html') as file:
         page = file.read()
 
-    output = f'<script>\n{script}\n</script>\n{page}'
+    output = f'<style>\n{style}\n</style>\n\n<script>\n{script}\n</script>\n\n{page}'
 
     with open('output.html', 'w') as file:
         file.write(output)
