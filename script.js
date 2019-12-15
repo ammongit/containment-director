e6ff482151c15d3f73f46a0ed628099933fd5692
Ammon Smith <ammon.i.smith@gmail.com>
2019-12-15 17:55:00 -0500

    Add address generator.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Transitions
 function intro2() {
   document.getElementById('intro1').style = 'display: none';
   document.getElementById('intro2').style = '';
@@ -6,4 +7,120 @@ function intro2() {
 function intro3() {
   document.getElementById('intro2').style = 'display: none';
   // TODO
+}
+
+// Anomalies
+var STREETS = [
+  'York Avenue',
+  'First Avenue',
+  'Second Avenue',
+  'Third Avenue',
+  'Lexington Avenue',
+  'Madison Avenue',
+  'Park Avenue',
+  'Fifth Avenue',
+  'Sixth Avenue',
+  'Seventh Avenue',
+  'Central Park West',
+  'Columbus Avenue',
+  'Amsterdam Avenue',
+  'West End Avenue',
+  'West Broadway',
+  'Broadway',
+  'Allen Street',
+  'Astor Place',
+  'Beach Street',
+  'Bleecker Street',
+  'Broome Street',
+  'Columbus Circle',
+  'Cortlandt Street',
+  'Delancey Street',
+  'Fulton Street',
+  'Nassau Street',
+  'Rutgers Street',
+  'Sullivan Street',
+  'Thompson Street',
+  'Vesey Street',
+  'Wooster Street',
+  'Clark Street',
+  'DeKalb Avenue',
+  'Vanderbilt Avenue',
+  'Roosevelent Avenue',
+  'Albany Street',
+  'Bowery',
+  'Canal Street',
+  'Chambers Street',
+  'Cherry Street',
+  'Christopher Street',
+  'Franklin Street',
+  'Gold Street',
+  'Houston Street',
+  'Hudson Street',
+  'John Street',
+  'Lafayette Street',
+  'Pearl Street',
+  'Wall Street',
+  'Water Street',
+  'Worth Street',
+];
+
+var PLACES = [
+  'Albee Square',
+  'Chatham Square',
+  'Duffy Square',
+  'Hanover Square',
+  'Herald Square',
+  'Lincoln Square',
+  'Madison Square Garden',
+  'Times Square',
+  'Tompkins Square Park',
+  'Verdi Square',
+  'Washington Square Park',
+  'Worth Square',
+  'Governor\'s Island',
+  'Liberty Island',
+  'Roosevelt Island',
+];
+
+function randomAddress() {
+  if (Math.random() < 0.1) {
+    return randElement(PLACES);
+  }
+
+  var street;
+  if (Math.random() < 0.4) {
+    // Named street
+    street = randElement(STREETS);
+  } else {
+    // Numbered street, with E/W
+    var number = randRange(200, 1);
+    if (number < 110) {
+      var direction = randRange(1) ? 'West' : 'East';
+      number = direction + ' ' + number;
+    }
+    street = number + numericSuffix(number) + ' Street';
+  }
+
+  return randRange(3000, 1) + ' ' + street;
+}
+
+
+// Helpers
+function numericSuffix(number) {
+  switch (number % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    default:
+      return 'th';
+  }
+}
+
+function randRange(upper, lower = 0) {
+  return Math.floor(Math.random() * (upper - lower)) + lower;
+}
+
+function randElement(array) {
+  return array[randRange(array.length)];
 }
