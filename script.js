2794929e171b4496fe219384a296e398fb84c8fa
Ammon Smith <ammon.i.smith@gmail.com>
2019-12-21 23:41:40 -0800

    Revert "Don't delete anomaly from list."
    This reverts commit 8e277151fd7251815c8de6a0e8115e633052394c.

--- script.js
+++ script.js
@@ -208,6 +208,8 @@ function clearCurrentAnomaly() {
     ]);
   }
 
+  var number = context.anomaly.number;
+  delete anomalies[number];
   context.anomaly = null;
   context.costs = {
     waste: 0,
